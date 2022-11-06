47c0e36a7c3bf5cd082558a25e5d1df0e1d06455
guadaubina <102978507+guadaubina@users.noreply.github.com>
2022-11-06 18:47:02 -0300

    a

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
+import mock.mock_manager as mock
 
 app = Flask(__name__)
+CORS(app)
 
+@app.route('/api/pedidos-ayuda/', methods=['GET'])
+def get_ayuda():
+    print("Hola")
+    return mock.read_mock_json(mock.MOCK_ID_GET_ALL)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/api/pedidos-ayuda/', methods=['POST'])
+def post_ayuda():
+    data = request.get_json()
+    list.append(data)
+    return mock.read_mock_json(mock.MOCK_ID_POST)
 
-
-if __name__ == '__main__':
+if __name__ == '__app__':
     app.run()
