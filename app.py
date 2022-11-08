f2d1248ce41094c46536f2d9a037b1e11f0a6c87
guadaubina <102978507+guadaubina@users.noreply.github.com>
2022-11-08 17:47:42 -0300

    a

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
 from flask import Flask
 from flask_cors import CORS
-import mock.mock_manager as mock
+from flask import request
+from flask import jsonify
+from db.db_manager import save_new_consulta, all_consultas
+import random
+
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/api/pedidos-ayuda/', methods=['GET'])
-def get_ayuda():
-    print("Hola")
-    return mock.read_mock_json(mock.MOCK_ID_GET_ALL)
+@app.route('/forms', methods=['POST'])
+def new_consulta():
+    req_json = request.json
+
+    consulta_id = random.randint(100, 10000)
+    req_json["consulta_id"] = consulta_id
+    save_new_consulta(req_json)
+
+    return {
+        "consulta_id": str(consulta_id)
+    }
 
 
-@app.route('/api/pedidos-ayuda/', methods=['POST'])
-def post_ayuda():
-    data = request.get_json()
-    list.append(data)
-    return mock.read_mock_json(mock.MOCK_ID_POST)
+@app.route("/forms", methods=["GET"])
+def show_consultas():
+    return all_consultas()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
